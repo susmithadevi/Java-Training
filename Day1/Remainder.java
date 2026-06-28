a28f41d1061ce82ed3ebda8eae08b7f70e43f218
susmithadevi <susmithadevi1512@gmail.com>
2026-06-28 14:15:50 +0530

    Initial push

---
+++ Day1/Remainder.java
@@ -0,0 +1,9 @@
+public class Remainder {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 80;
+        int remain = a%b;
+        System.out.println("The remainder is: "+ remain);
+    }
+    
+}
